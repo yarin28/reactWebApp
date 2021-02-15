c78ad50af9017afee1e9f5fdc70b17028c97e65b
Yarin <yarin.greenfeld@gmail.com>
2021-02-15 14:12:50 +0200

    added React router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ComponentType } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+import HomePage from "./Componets/Pages/HomePage"
 import './App.css';
+import Dashboard from './Componets/Dashboard';
+import Chart from './Componets/Chart';
+import NavBar from './Componets/NavBar'
+import { useStyles } from './App.styles'
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
+import { theme } from "./theme"
 
-function App() {
+
+const App: ComponentType = () => {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline></CssBaseline>
+      <Router>
+        <NavBar />
+        <div className={classes.appBarSpacer}></div>
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/dash" exact>
+            <Dashboard />
+          </Route>
+          <Route path="/chart" exact>
+            <Chart />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
