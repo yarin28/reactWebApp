a1c80ad274a4c0250e2a970c950f5424bbe0002a
Yarin <yarin.greenfeld@gmail.com>
2021-02-15 14:11:09 +0200

    added theme for the app

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { createMuiTheme } from "@material-ui/core";
+
+import yellow from '@material-ui/core/colors/yellow';
+import orange from '@material-ui/core/colors/orange';
+
+export const theme = createMuiTheme({
+    palette: {
+        type: "dark",
+        primary: yellow,
+        secondary: orange,
+
+    }
+
+
+})
