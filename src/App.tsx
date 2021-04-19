cd3d2d48642cb0907744d9af3020a658baded041
Yarin <yarin.greenfeld@gmail.com>
2021-04-19 21:27:34 +0300

    add the new app router

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,17 @@ import './App.css';
 import Dashboard from './Componets/Dashboard';
 import Chart from './Componets/Chart';
 import NavBar from './Componets/NavBar'
+import StatusInfo from './Componets/StatusInfo'
 import { useStyles } from './App.styles'
 import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import { theme } from "./theme"
+import Brighness from './Componets/Pages/Brightness'
+import Humidity from './Componets/Pages/Humidity'
+import Temperture from './Componets/Pages/Temperature'
+import SettingsBrightnessIcon from '@material-ui/icons/SettingsBrightness';
+import Door from './Componets/Door';
+import DoorPage from './Componets/Pages/DoorPage';
+import SetInterval from './Componets/Pages/SetInterval';
 
 
 const App: ComponentType = () => {
@@ -25,13 +33,31 @@ const App: ComponentType = () => {
         <div className={classes.appBarSpacer}></div>
         <Switch>
           <Route path="/" exact>
-            <HomePage />
+           <HomePage />
           </Route>
           <Route path="/dash" exact>
             <Dashboard />
           </Route>
           <Route path="/chart" exact>
-            <Chart name="water graph" link="waterGraph" />
+            {/* <Chart name="brightness" link="brightness" /> */}
+          </Route>
+          <Route path="/humidity" exact>
+            <Humidity></Humidity>
+          </Route>
+          <Route path="/temperature" exact>
+            <Temperture></Temperture>
+          </Route>
+          <Route path="/brightness" exact>
+            <Brighness></Brighness>
+          </Route>
+          <Route path="/dashBoard" exact>
+            <Dashboard></Dashboard>
+          </Route>
+          <Route path="/door" exact>
+            <DoorPage></DoorPage>
+          </Route>
+          <Route path="/interval" exact>
+      <SetInterval ip="192.168.1.15:8090" place="/interval/" query="?set=" name="interval"></SetInterval>
           </Route>
         </Switch>
       </Router>
