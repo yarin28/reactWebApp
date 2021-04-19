f40c04115cc3efa5e7d2e24e690152fcb69624f0
Yarin <yarin.greenfeld@gmail.com>
2021-04-19 21:32:36 +0300

    temp comp

---
+++ src/Componets/Pages/Temperature/Temperature.tsx
@@ -0,0 +1,20 @@
+import React, { ComponentType, useCallback, useState } from "react"
+import SendToServer from "../../SendToServer";
+import  Chart from "../../Chart";
+import { withRouter } from "react-router-dom";
+const Temperature:ComponentType = ( props:any )=> {
+    const [xyCords, xyCordSet] = useState([{}]);
+    return (
+        <div>
+            <Chart name="temperature" link="temperature" cords={xyCords}
+            setCords={xyCordSet}/>
+            <SendToServer name="get days" link="temperature" query="/?days="
+            cords={xyCords} setCords={xyCordSet}></SendToServer>
+            <SendToServer name="get hours" link="temperature" query="/?hours="
+            cords={xyCords} setCords={xyCordSet}></SendToServer>
+            <SendToServer name="get weeks" link="temperature" query="/?weeks="
+            cords={xyCords} setCords={xyCordSet}></SendToServer>
+        </div>
+    )
+}
+export default withRouter(Temperature);
