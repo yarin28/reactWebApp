d69bf2d29a1dcd312f9c9beaafa4f091af647148
Yarin <yarin.greenfeld@gmail.com>
2021-04-19 21:28:11 +0300

    is better without all the misc stuff

--- src/Componets/Chart/Chart.tsx
+++ src/Componets/Chart/Chart.tsx
@@ -1,37 +1,43 @@
-
 import React, { ComponentType, useEffect, useMemo, useRef, useState } from "react"
 import { Scatter } from "react-chartjs-2"
 import { Button } from "@material-ui/core";
 import { w3cwebsocket as WebSocket } from 'websocket'
 const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
-interface ChartProps { name: string; link: string }
+interface ChartProps { name: string; link: string ;cords:any;setCords:any}
 const Chart: ComponentType<ChartProps> = (props) => {
+    const getDays = async (param: string) => {
+        const response = await fetch("http://192.168.1.15:8090/" + props.link + param);
+        console.log(response);
+        const data = await response.json();
+        const realy_json = JSON.parse(data)
+        console.log(data);
+        console.log(Date.parse(realy_json[0].x));
+        for (let cord of realy_json) {
+            addXY({ x: Date.parse(cord.x), y: cord.y });
+        }
 
-    const [xyCords, xyCordSet] = useState([{
-        x: -10,
-        y: 0
-    }, {
-        x: 0,
-        y: 10
-    }, {
-        x: 10,
-        y: 5
-    }]);
-
-    const client = useRef<null | WebSocket>(null);
-    useEffect(() => {
-        client.current = new WebSocket("ws://192.168.1.13:8090/" + props.link);
-        // will have to be an prop
-    }, []);
-    useEffect(() => {
-        if (client.current)
-            client.current.onmessage = (message: any) => {
-                const dataArray = JSON.parse(message.data);
-                for (let obj of dataArray) {
-                console.log(obj);
-                xyCordSet([...xyCords, { x: obj.x, y: obj.y }]);}
-            };
-    }, [xyCords]);
+    };
+    // const client = useRef<null | WebSocket>(null);
+    // useEffect(() => {
+    //     client.current = new WebSocket("ws://192.168.1.15:8090/" + props.link+ "/register");
+    //     // will have to be an prop
+    // }, []);
+    // useEffect(() => {
+    //     if (client.current)
+    //         client.current.onmessage = (message: any) => {
+    //             const dataArray = JSON.parse(message.data);
+    //             console.log(dataArray);
+    //             try{
+    //             for (let obj of dataArray) {
+    //                 console.log(obj);
+    //                 props.setCords([...props.cords, { x: obj.x, y: obj.y }]);
+    //             }}
+    //             catch(e){
+    //                 console.log("cought the not irtratble shit1")
+    //                 props.setCords([...props.cords, { x: Date.parse(dataArray.x), y: dataArray.y }]);
+    //             }
+    //         };
+    // }, [props.cords]);
     /**
      * Generates a random integer between min and max (inclusive)
      * @param  {number} min
@@ -48,7 +54,7 @@ const Chart: ComponentType<ChartProps> = (props) => {
         return value < 0 ? 'red' : 'green';
     };
     const addXY = async (xy: { x: number; y: number; }) => {
-        xyCordSet(oldArray => [...oldArray, xy]);
+        props.setCords((oldArray:any)=> [...oldArray, xy]);
     };
     const add10XY = async () => {
         for (let i = 0; i < 100; i++) {
@@ -63,7 +69,7 @@ const Chart: ComponentType<ChartProps> = (props) => {
         datasets: [
             {
                 label: 'good boy',
-                data: xyCords,
+                data: props.cords,
                 borderColor: 'white',
                 borderWidth: 2,
                 pointBackgroundColor: (context: any) => { return chooseColorSign(context) },
@@ -75,11 +81,12 @@ const Chart: ComponentType<ChartProps> = (props) => {
                 showLine: true
             }
         ]
-    }), [xyCords])
+    }), [props.cords])
     return (
         <div>
             <Scatter data={chartData} />
             <Button onClick={() => add10XY()}>test</Button>
+            <Button onClick={() => getDays("/?days=10")} >add days data</Button>
         </div >
     )
 }
